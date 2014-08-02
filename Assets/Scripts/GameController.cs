17ed29e71d514ff9c89e988a319b689ec7000b4f
Nazware <wlfxnaz@gmail.com>
2014-08-02 16:44:00 +0900

    タッチorクリックした地点を取得

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class GameController : MonoBehaviour
 {
@@ -15,6 +16,46 @@ public class GameController : MonoBehaviour
 	
 	void Update ()
     {
-	
+        var inputs = getInputPositions();
+
+        if (inputs.Count > 0)
+        {
+            foreach (var input in inputs)
+            {
+                Ray ray = Camera.main.ScreenPointToRay(input);
+                RaycastHit hit = new RaycastHit();
+
+                if (Physics.Raycast(ray, out hit))
+                {
+                    var obj = hit.collider.gameObject;
+                    onTouch(input, obj);
+                }
+            }
+        }
 	}
+
+    void onTouch(Vector3 touchPos, GameObject touchObject)
+    {
+        Debug.Log(touchPos + ", " + touchObject.name);
+    }
+
+    List<Vector3> getInputPositions()
+    {
+        var inputPositions = new List<Vector3>();
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            inputPositions.Add(Input.mousePosition);
+        }
+
+        foreach (var touch in Input.touches)
+        {
+            if (touch.phase == TouchPhase.Began)
+            {
+                inputPositions.Add(new Vector3(touch.position.x, touch.position.y, 0.0f));
+            }
+        }
+
+        return inputPositions;
+    }
 }
