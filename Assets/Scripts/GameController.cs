98605e203b6956d52b5b7c80960de534ba86bd13
Nazware <wlfxnaz@gmail.com>
2014-08-02 16:07:12 +0900

    仮画面を作成

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour
+{
+    [SerializeField]
+    GameObject yesButton;
+    [SerializeField]
+    GameObject noButton;
+
+	void Start ()
+    {
+	
+	}
+	
+	void Update ()
+    {
+	
+	}
+}
