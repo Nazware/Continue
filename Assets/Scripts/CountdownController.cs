960310a9e3cdbf6c13faee20a2872e8eda6b47ca
Nazware <wlfxnaz@gmail.com>
2014-08-03 03:20:32 +0900

    カウントダウン用のPrefabを追加

---
+++ Assets/Scripts/CountdownController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class CountdownController : MonoBehaviour
+{
+    [SerializeField]
+    NumberView numberView;
+    [SerializeField]
+    int countSecond;
+
+    DateTime target;
+
+	void Start ()
+    {
+        target = DateTime.Now;
+        target = target.AddSeconds(countSecond);
+	}
+	
+	void Update ()
+    {
+        var remainingTime = target - DateTime.Now;
+        numberView.SetData(remainingTime.Seconds);
+	}
+}
---
+++ Assets/Scripts/NumberView.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NumberView : MonoBehaviour
+{
+    const float MarginU = 0.1f;
+ 
+    public void SetData(int number)
+    {
+        number %= 10;
+
+        renderer.material.SetTextureOffset("_MainTex", new Vector2(MarginU * number, 0));
+    }
+}
