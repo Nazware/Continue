057d20b906f788592f2125de3705265ce1c9f428
大塚 義弥 <josyua@ootsuka-no-MacBook-Pro.local>
2014-08-03 00:11:06 +0900

    ステージ５まで追加

--- Assets/Scripts/Stages/Stage4.cs
+++ Assets/Scripts/Stages/Stage4.cs
@@ -10,18 +10,28 @@ public class Stage4 : BaseStage
 	GameObject yesButtonPrefab;
 	[SerializeField]
 	GameObject noButtonPrefab;
+	[SerializeField]
+	float rotateSpeed = 0.05f;
+
+	private GameObject yesButton;
+	private float thetaRad = 0.0f;
 
 	// Use this for initialization
 	void Start () {
 		CreateStageObject(titlePrefab, new Vector3(0, 200, -2));
-		CreateStageObject(yesButtonPrefab, new Vector3(100f, 0.0f, -3.0f));
+		yesButton = CreateStageObject(yesButtonPrefab, new Vector3(100f, 0.0f, -4.0f));
 		CreateStageObject(noButtonPrefab, new Vector3(Random.Range(-200.0f, -50.0f),
 		                                              Random.Range(-130.0f, 100.0f),
-		                                              -4));		
+		                                              -3));		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		thetaRad += rotateSpeed;
+		if(thetaRad > 360.0f){
+			thetaRad = 0.0f;
+		}
+
+		yesButton.transform.position = new Vector3(250.0f * Mathf.Cos(thetaRad),-50.0f + (150.0f * Mathf.Sin(thetaRad)), 0.0f);
 	}
 }
