8fccd3bc9f6eabc10f3b0e08fd793f1ccc616e60
大塚 義弥 <josyua@ootsuka-no-MacBook-Pro.local>
2014-08-02 23:06:07 +0900

    ステージ３追加

--- Assets/Scripts/GUIEvent/YesEventStage3.cs
+++ Assets/Scripts/GUIEvent/YesEventStage3.cs
@@ -6,16 +6,20 @@ public class YesEventStage3 : BaseEvent {
 	[SerializeField]
 	float yesSpeed = 100.0f;
 	[SerializeField]
-	float addReturnTime = 3.0f;
-	//private float 
+	float addTime = 3.0f;
+	private float nextReturnTime = 0.0f;
 
 	// Use this for initialization
 	void Start () {
-	
+		nextReturnTime = Time.time + addTime;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if(Time.time > nextReturnTime){
+			yesSpeed = yesSpeed * -1.0f;
+		}
+
 		this.gameObject.transform.Translate(10.0f * yesSpeed * Time.deltaTime, 0.0f, 0.0f);
 	}
 
