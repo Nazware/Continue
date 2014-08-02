d33dc5caa6e04f1f11c4633418d30d3aafb2ae6a
大塚 義弥 <josyua@ootsuka-no-MacBook-Pro.local>
2014-08-03 04:20:36 +0900

    パーティクル入りました

--- Assets/Scripts/GUIEvent/YesEvent.cs
+++ Assets/Scripts/GUIEvent/YesEvent.cs
@@ -3,10 +3,11 @@ using System.Collections;
 
 public class YesEvent : BaseEvent {
 
+	private GameObject tapParticle;
 	// Use this for initialization
 	void Start ()
     {
-	
+		tapParticle = Resources.Load("particle/Particle System") as GameObject;
 	}
 	
 	// Update is called once per frame
@@ -17,6 +18,7 @@ public class YesEvent : BaseEvent {
 
 	public override void onTap(Vector3 touchPos)
     {
+		Instantiate(tapParticle, gameObject.transform.position, tapParticle.transform.rotation);
         gameObject.SetActive(false);
         gameController.GoesNextStage();
 	}
