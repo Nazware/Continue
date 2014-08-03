1fc815615b577abc5034a0499c83bd8a419b0f48
Nazware <wlfxnaz@gmail.com>
2014-08-03 09:31:52 +0900

    タイマーが0になったらひとまずGameOver(Stage1から)になるように処理を追加

--- Assets/Scripts/CountdownController.cs
+++ Assets/Scripts/CountdownController.cs
@@ -3,21 +3,39 @@ using System.Collections;
 using System;
 
 public class CountdownController : MonoBehaviour
-{
+{    
     [SerializeField]
     NumberView numberView;
 
+    GameController gameController;
+
     DateTime target;
+    bool gameovered = false;
 
 	void Update ()
     {
-        var remainingTime = target - DateTime.Now;
+        var currentTime = DateTime.Now;
+        var remainingTime = target - currentTime;
+        if (target <= currentTime)
+        {
+            if (!gameovered)
+            {
+                gameController.GameOver();
+                gameovered = true;
+            }
+            remainingTime = new TimeSpan();
+        }
+
         numberView.SetData(remainingTime.Seconds);
 	}
 
-    public void Initialize(int countSeconds)
+    public void Initialize(int countSeconds, GameController gameController)
     {
         target = DateTime.Now;
         target = target.AddSeconds(countSeconds);
+
+        this.gameController = gameController;
+
+        gameovered = false;
     }
 }
